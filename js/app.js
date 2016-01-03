f8b542cf22a1526ccfe4bbe35ab8d83fd58e6501
Kresimir I. Nurnberger <kin9006@aol.com>
2016-01-03 18:56:55 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var numCount = 1
+while (numCount < 100) {
+	if (numCount % 15 == 0) {
+		console.log('Fizz Buzz');
+	}
+	else if (numCount % 3 == 0) {
+		console.log('Fizz');
+	}
+	else if (numCount % 5 == 0) {
+		console.log('Buzz');
+	}
+	else {
+		console.log(numCount);
+	}
+	numCount ++
+}
+
+
+// $(document).ready(function() {
+// 	$('button').click(function() {
+		
+// 	})
+// })
