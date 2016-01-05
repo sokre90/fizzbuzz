bcc72fae40f5e2538841b3806fa8c9f44a8cce18
Kresimir I. Nurnberger <kin9006@aol.com>
2016-01-05 12:46:13 +0100

    fizzbuzz app

--- js/app.js
+++ js/app.js
@@ -1,23 +1,25 @@
-var numCount = 1
-while (numCount <= 100) {
-	if (numCount % 15 == 0) {
-		console.log('FizzBuzz');
+$(document).ready(function() {
+	// var numCount = 1
+	$('#showfb').click(function() {
+		$('ul').empty();
+		var numCount = 1
+	while (numCount <= 100) {
+		if (numCount % 15 == 0) {
+			$('ul').append('<li>FizzBuzz</li>');
+		}
+		else if (numCount % 3 == 0) {
+			$('ul').append('<li>Fizz</li>');
+		}
+		else if (numCount % 5 == 0) {
+			$('ul').append('<li>Buzz</li>');
+		}
+		else {
+			$('ul').append('<li>'+numCount+'</li>');
+		}
+		numCount ++
 	}
-	else if (numCount % 3 == 0) {
-		console.log('Fizz');
-	}
-	else if (numCount % 5 == 0) {
-		console.log('Buzz');
-	}
-	else {
-		console.log(numCount);
-	}
-	numCount ++
-}
-
-
-// $(document).ready(function() {
-// 	$('button').click(function() {
-		
-// 	})
-// })
+	})
+	$('#hidefb').click(function() {
+		$('ul').empty();
+	})
+})
