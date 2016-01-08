fcd0da7a95e9e54930a02d352cf35a2ad9c849f1
Kresimir I. Nurnberger <kin9006@aol.com>
2016-01-08 16:59:34 +0100

    refactored fizzbuzz

--- js/app.js
+++ js/app.js
@@ -1,25 +1,54 @@
 $(document).ready(function() {
-	// var numCount = 1
+	$('input').keyup(function() {
+		value = $(this).val();
+		numCheck = isNaN(value);
+		if (numCheck == false) {
+			$('p').text(value);
+		}
+		else if (numCheck == true) {
+			$('p').text("Enter again, please!");
+		}
+	})
+
 	$('#showfb').click(function() {
-		$('ul').empty();
-		var numCount = 1
-	while (numCount <= 100) {
-		if (numCount % 15 == 0) {
-			$('ul').append('<li>FizzBuzz</li>');
-		}
-		else if (numCount % 3 == 0) {
-			$('ul').append('<li>Fizz</li>');
-		}
-		else if (numCount % 5 == 0) {
-			$('ul').append('<li>Buzz</li>');
-		}
-		else {
-			$('ul').append('<li>'+numCount+'</li>');
+		var numCount = 1;
+		while (numCount <= value) {
+			if (numCount % 15 == 0) {
+				$('ul').append('<li>FizzBuzz</li>');
+			}
+			else if (numCount % 3 == 0) {
+				$('ul').append('<li>Fizz</li>');
+			}
+			else if (numCount % 5 == 0) {
+				$('ul').append('<li>Buzz</li>');
+			}
+			else {
+				$('ul').append('<li>'+numCount+'</li>');
+			}
 		}
 		numCount ++
-	}
-	})
-	$('#hidefb').click(function() {
-		$('ul').empty();
-	})
+	})			
 })
+// $(document).ready(function() {
+// $('#showfb').click(function() {
+	// 	$('ul').empty();
+	// 	var numCount = 1
+	// while (numCount <= 100) {
+	// 	if (numCount % 15 == 0) {
+	// 		$('ul').append('<li>FizzBuzz</li>');
+	// 	}
+	// 	else if (numCount % 3 == 0) {
+	// 		$('ul').append('<li>Fizz</li>');
+	// 	}
+	// 	else if (numCount % 5 == 0) {
+	// 		$('ul').append('<li>Buzz</li>');
+	// 	}
+	// 	else {
+	// 		$('ul').append('<li>'+numCount+'</li>');
+	// 	}
+	// 	numCount ++
+	// }
+	// })
+	// $('#hidefb').click(function() {
+	// 	$('ul').empty();
+	// })
